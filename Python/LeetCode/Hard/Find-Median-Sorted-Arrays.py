76b40419b68b41cdf60ff5247a462ec9f49e04d3
YANG XU <yaxu2@student.unimelb.edu.au>
2019-07-06 21:04:50 +1000

    [update]

--- Python/LeetCode/Hard/Find-Median-Sorted-Arrays.py
+++ Python/LeetCode/Hard/Find-Median-Sorted-Arrays.py
@@ -1,6 +1,3 @@
-# Improving
-
-
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         h1 = 0
@@ -9,29 +6,30 @@ class Solution:
         counter = 0
         if nums1[h1] > nums2[h2]:
             target[counter] = nums2[h2]
+            h2 += 1
             counter += 1
-            for value in nums2[h2+1:]:
+            for value in nums2[h2:]:
                 if value < nums1[h1]:
                     target[counter] = value
                     counter += 1
                 else:
                     target[counter] = nums1[h1]
+                    h1 += 1
                     counter += 1
                     break
-            h2 += 1
         else:
             target[counter] = nums1[h1]
+            h1 += 1
             counter += 1
-            for value in nums1[h1+1:]:
+            for value in nums1[h1:]:
                 if value < nums2[h2]:
                     target[counter] = value
                     counter += 1
                 else:
                     target[counter] = nums2[h2]
+                    h2 += 1
                     counter += 1
                     break
-            h1 += 1
-
         print(target)
 # 0 1 5
 # 2 4 6
