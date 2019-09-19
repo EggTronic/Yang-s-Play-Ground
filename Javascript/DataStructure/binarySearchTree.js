22d479bd3ab13b30fadb2c187b92d31b58221036
YANG XU <yaxu2@student.unimelb.edu.au>
2019-09-19 23:58:41 +1000

    [update] binary search tree

---
+++ Javascript/DataStructure/binarySearchTree.js
@@ -0,0 +1,63 @@
+function Start() {
+  let bst = new BinarySearchTree();
+  for (i = 0; i <= 10; i++) {
+    bst.insert(new Node(Math.random() * 10));
+  }
+  console.log(bst.LMR());
+}
+
+function BinarySearchTree() {
+  this.root = null;
+}
+
+function Node(val) {
+  this.val = val;
+  this.left = null;
+  this.right = null;
+}
+
+// 插入
+BinarySearchTree.prototype.insert = function (n) {
+  if (this.root == null) {
+    this.root = n;
+  } else {
+    let parent = null;
+    let temp = this.root;
+    while (temp != null) {
+      if (n.val < temp.val) {
+        parent = temp;
+        temp = temp.left;
+      } else if (n.val > temp.val) {
+        parent = temp;
+        temp = temp.right;
+      }
+    }
+    if (n.val < parent.val) {
+      parent.left = n;
+    } else if (n.val > parent.val) {
+      parent.right = n;
+    }
+  }
+}
+
+// 中序遍历
+BinarySearchTree.prototype.LMR = function () {
+  let temp = this.root;
+  let stack = [];
+  let result = [];
+
+  while (stack.length > 0 || temp) {
+    if (temp) {
+      stack.push(temp);
+      temp = temp.left;
+    } else {
+      temp = stack.pop();
+      result.push(temp.val);
+      temp = temp.right
+    }
+  }
+  return result;
+}
+
+
+Start();
---
+++ Python/Advanced-Algorithm/dictCut.py
@@ -0,0 +1,15 @@
+# input a string and a dict of string
+# return true if string can be splited into the words in the dict
+# words can be repeated
+
+
+def dictCut(word, dict):
+
+
+if __name__ == "__main__":
+    word = 'awesome'
+    dict = {
+        'awe',
+        'som',
+        'me'
+    }
