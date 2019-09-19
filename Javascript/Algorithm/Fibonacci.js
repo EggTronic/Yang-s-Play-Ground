dd1dc4f22f9096ea37b9df9ebd0a4c810fc584c8
YANG XU <yaxu2@student.unimelb.edu.au>
2019-09-20 04:10:20 +1000

    [practice] algorithm fibonacii

---
+++ Javascript/Algorithm/Fibonacci.js
@@ -0,0 +1,39 @@
+let store = {};
+store["0"] = 0;
+store["1"] = 1;
+
+function Fibonacci(n) {
+    if (n == 1 || n == 0) {
+        return n;
+    } else {
+        let f1 = 0;
+        let f2 = 0;
+        if ((n - 1).toString() in store) {
+            f1 = store[(n - 1).toString()];
+        } else {
+            f1 = Fibonacci(n - 1);
+            store[(n - 1).toString()] = f1;
+        }
+        if ((n - 2).toString() in store) {
+            f2 = store[(n - 2).toString()];
+        } else {
+            f2 = Fibonacci(n - 2)
+            store[(n - 2).toString()] = f2;
+        }
+
+        return (f1 + f2);
+    }
+}
+
+function Fibonacci2(n) {
+    var f = 0, g = 1;
+    while (n--) {
+        g += f;
+        f = g - f;
+    }
+    return f;
+}
+
+
+console.log(Fibonacci(10));
+console.log(Fibonacci2(10));
